63d2f6d4f1507e6949bf2d37b4fb96516c126c7a
imranshaikmuma <imranshaik@mail.usf.edu>
2017-02-08 16:07:01 -0500

    nltk basics

---
+++ nltkbasic.py
@@ -0,0 +1,16 @@
+import nltk
+
+#nltk.download()
+# tokenizer are of two types:
+# word tokenizer
+# sentence tokenizer
+
+# corpora - a body of text eg: speeches etc
+
+# lexicon  is a dictionary containing word and meanings
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = "hello imran, what are you doing?"
+
+
