8d9e55eb4ba99d067ade2fbfa3c8dd9c644e2566
imranshaikmuma <imranshaik@mail.usf.edu>
2017-02-11 01:21:25 -0500

    classification

--- classify.py
+++ classify.py
@@ -5,21 +5,15 @@ import random #to shuffle up dataset
 from nltk.corpus import movie_reviews
 
 #print(movie_reviews)
-#documents = []
-
-#for category in movie_reviews.categories():
-	# for fileid in movie_reviews.fileids(category):
-		# documents.append[list(movie_reviews.words(fileid)),category)]
-		
 		
 documents = [(list(movie_reviews.words(fileid)),category)
 			for category in movie_reviews.categories()
 			for fileid in movie_reviews.fileids(category)]
 			
 			
-random.shuffle(documents)
+# random.shuffle(documents)
 
-#print(documents)
+print(documents[1])
 
 all_words = []
 for w in movie_reviews.words():
